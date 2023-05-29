badc6d1e199f239a327c227fda63d4a9e0572d6c
root <root@vladybarvy.localdomain>
2023-05-29 20:33:48 +0300

    solution number 1

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const array = [[12,3,4],[0,0,0],[23,5,6],[123,'6',-5],[0,'0','O',1],['test']]
 // если вы встречаете цифру в виде строки, ее нужно нормализовать и посчитать тоже
  
 // выведите итоговый массив в консоль
+const greatSumma = array.map((el) => {
+    let sum = 0;
+    for (let i of el) {
+        sum += Number(i);
+    }
+    return sum;
+});
+
+console.log(greatSumma);
